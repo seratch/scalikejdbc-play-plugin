c2428030cca9ba745c540431327147ca0ea8e884
Kazuhiro Sera <seratch@gmail.com>
2012-05-22 11:43:44 +0900

    Fixed issue #3 Cannot work with default DB plugin because of 'db.global.xxx' configuration

--- src/main/scala/scalikejdbc/PlayPlugin.scala
+++ src/main/scala/scalikejdbc/PlayPlugin.scala
@@ -16,68 +16,63 @@
 package scalikejdbc
 
 import play.api._
-import scalikejdbc._
 
 /**
  * The Play plugin to use ScalikeJDBC
  */
 class PlayPlugin(app: Application) extends Plugin {
 
-  private lazy val config = app.configuration.getConfig("db").getOrElse(Configuration.empty)
+  private lazy val dbConfig = app.configuration.getConfig("db").getOrElse(Configuration.empty)
 
-  private def configValueOptional(name: String, key: String): Option[String] = {
-    config.getString(name + "." + key)
+  private def opt(name: String, key: String)(implicit config: Configuration): Option[String] = {
+    dbConfig.getString(name + "." + key)
   }
 
-  private def configValue(name: String, key: String): String = {
-    config.getString(name + "." + key) getOrElse {
-      throw config.reportError(name, "Missing configuration [db." + name + "." + key + "]")
+  private def require(name: String, key: String)(implicit config: Configuration): String = {
+    dbConfig.getString(name + "." + key) getOrElse {
+      throw dbConfig.reportError(name, "Missing dbConfiguration [db." + name + "." + key + "]")
     }
   }
 
-  config.subKeys map { name =>
+  dbConfig.subKeys map {
+    name =>
+      def load(name: String): (String, String, String, ConnectionPoolSettings) = {
+        implicit val config = dbConfig
+        Class.forName(require(name, "driver"))
+        val default = new ConnectionPoolSettings
+        val settings = new ConnectionPoolSettings(
+          initialSize = opt(name, "poolInitialSize").map(v => v.toInt).getOrElse(default.initialSize),
+          maxSize = opt(name, "poolMaxSize").map(v => v.toInt).getOrElse(default.maxSize),
+          validationQuery = opt(name, "poolValidationQuery").getOrElse(default.validationQuery)
+        )
+        (require(name, "url"), opt(name, "user").getOrElse(""), opt(name, "password").getOrElse(""), settings)
+      }
 
-    def load(name: String): (String, String, String, ConnectionPoolSettings) = {
-      // load the jdbc driver
-      configValueOptional(name, "driver") map { driver => Class.forName(driver) }
+      name match {
+        case "default" =>
+          val (url, user, password, settings) = load(name)
+          ConnectionPool.singleton(url, user, password, settings)
+        case _ =>
+          val (url, user, password, settings) = load(name)
+          ConnectionPool.add(Symbol(name), url, user, password, settings)
+      }
 
-      val url = configValue(name, "url")
-      val user = configValueOptional(name, "user") getOrElse ("")
-      val password = configValueOptional(name, "password") getOrElse ("")
+  }
 
-      val defaultSettings = new ConnectionPoolSettings
-      val poolInitialSize: Int = configValueOptional(name, "poolInitialSize") map (v => v.toInt) getOrElse (defaultSettings.initialSize)
-      val poolMaxSize: Int = configValueOptional(name, "poolMaxSize") map (v => v.toInt) getOrElse (defaultSettings.maxSize)
-      val poolValidationQuery = configValueOptional(name, "poolValidationQuery") getOrElse (defaultSettings.validationQuery)
-      val settings = new ConnectionPoolSettings(
-        initialSize = poolInitialSize,
-        maxSize = poolMaxSize,
-        validationQuery = poolValidationQuery)
-      (url, user, password, settings)
-    }
+  private lazy val globalConfig = app.configuration.getConfig("scalikejdbc.global").getOrElse(Configuration.empty)
 
-    name match {
-      case "global" =>
-        // loading GlobalSettings
-        val globalLoggingSQLAndTime = "global.loggingSQLAndTime"
-        configValueOptional(globalLoggingSQLAndTime, "enabled").map(_.toBoolean) map { enabled =>
-          val default = new LoggingSQLAndTimeSettings
-          GlobalSettings.loggingSQLAndTime = new LoggingSQLAndTimeSettings(
-            enabled = enabled,
-            logLevel = configValueOptional(globalLoggingSQLAndTime, "logLevel").map(v => Symbol(v)).getOrElse(default.logLevel),
-            warningEnabled = configValueOptional(globalLoggingSQLAndTime, "warningEnabled").map(_.toBoolean).getOrElse(default.warningEnabled),
-            warningThresholdMillis = configValueOptional(globalLoggingSQLAndTime, "warningThresholdMillis").map(_.toLong).getOrElse(default.warningThresholdMillis),
-            warningLogLevel = configValueOptional(globalLoggingSQLAndTime, "warningLogLevel").map(v => Symbol(v)).getOrElse(default.warningLogLevel)
-          )
-        }
-      case "default" =>
-        val (url, user, password, settings) = load(name)
-        ConnectionPool.singleton(url, user, password, settings)
-      case _ =>
-        val (url, user, password, settings) = load(name)
-        ConnectionPool.add(Symbol(name), url, user, password, settings)
-    }
-
+  private val loggingSQLAndTime = "loggingSQLAndTime"
+  opt(loggingSQLAndTime, "enabled")(globalConfig).map(_.toBoolean).foreach {
+    enabled =>
+      implicit val config = globalConfig
+      val default = new LoggingSQLAndTimeSettings
+      GlobalSettings.loggingSQLAndTime = new LoggingSQLAndTimeSettings(
+        enabled = enabled,
+        logLevel = opt(loggingSQLAndTime, "logLevel").map(v => Symbol(v)).getOrElse(default.logLevel),
+        warningEnabled = opt(loggingSQLAndTime, "warningEnabled").map(_.toBoolean).getOrElse(default.warningEnabled),
+        warningThresholdMillis = opt(loggingSQLAndTime, "warningThresholdMillis").map(_.toLong).getOrElse(default.warningThresholdMillis),
+        warningLogLevel = opt(loggingSQLAndTime, "warningLogLevel").map(v => Symbol(v)).getOrElse(default.warningLogLevel)
+      )
   }
 
 }
